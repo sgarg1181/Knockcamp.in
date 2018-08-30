b407c11649935762292dd515af91c4edb037372f
sgarg1181 <sgarg1181@gmail.com>
2018-08-31 01:17:43 +0530

    Add files via upload

---
+++ css/dashboard.php
@@ -0,0 +1,47 @@
+.change{
+color:white;
+} 
+
+body{background-color: #08061e;}
+    .col-sm-10{
+ color:white;   
+}
+.table{
+ background-color: white;
+}
+#Side_Menu a{
+    color: azure;
+}
+#Side_Menu .active a{
+    color: #FFFFFF;
+    background-color: #27AAE1;
+    font-weight: bold;
+}
+#Side_Menu a:hover{
+    color: #ffffff;
+    background-color: #1ab394;
+    font-weight: bold;
+    display: block;
+}
+.col-sm-2 h1{
+    font-style: italic;
+    color: aliceblue;
+}
+.col-sm-2{margin-top: 30px;
+    background-color: #2f4050;
+        height: auto;}
+#Footer{
+    
+    padding:10px;
+    border-top: 1px solid black;
+    color:#eeeeee;
+    background-color: #211f22;
+    text-align: center;   
+}
+form{
+    font-size: 1.3em;
+    font-family: 'Concert One', cursive;
+}
+
+    
+  
